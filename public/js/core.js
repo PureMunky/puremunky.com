8fa63bdc7ab1c92aaa42b1f8bad69e2ee322aae3
Phil Corbett <corbett.phil@gmail.com>
2014-08-16 09:56:32 -0400

    styling and links

--- public/js/core.js
+++ public/js/core.js
@@ -1,10 +1,23 @@
 (function(ang) {
   'use strict';
 
-  var PureMunky = ang.module('PureMunky', []);
+  var PureMunky = ang.module('PureMunky', ['ngResource']);
+
+
+  /*
+  PureMunky.service('Twitch', ['$resource', function ($resource) {
+    return $resource('https://api.twitch.tv/kraken/streams/:userName', {}, {
+      query: {method:'GET'}
+    });
+  }]);
+  */
 
   PureMunky.controller('MainCtrl', ['$scope', function ($scope) {
     $scope.twitchName = 'puremunky';
+
+    $scope.twitchLive = function () {
+      return false;
+    };
 
   }]);
 
